715b140ec024820b565be5ea6a4d909aaf23432e
Valery Ilychev (Sarutobi) <ilychev@gmail.com>
2013-05-25 19:55:53 +0400

    Added settings for current database

--- Rynda/settings/dev_sarutobi.py
+++ Rynda/settings/dev_sarutobi.py
@@ -1,2 +1,16 @@
+# -*- coding: utf-8 -*-
+
 from .local import *
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        'NAME': 'rynda',
+        'USER': 'rynda',
+        'PASSWORD': 'rynda',
+        'HOST': 'localhost',
+        'PORT': '',
+    }
+}
 
